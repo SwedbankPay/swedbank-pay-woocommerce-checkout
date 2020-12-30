4571befc5788fe1aca4fc6cd9b4527a037595570
aait <info@aait.se>
2020-12-30 16:21:40 +0600

    Update version and metadata

--- swedbank-pay-woocommerce-checkout.php
+++ swedbank-pay-woocommerce-checkout.php
@@ -7,11 +7,11 @@
 	 * Author URI: https://profiles.wordpress.org/swedbankpay/
 	 * License: Apache License 2.0
 	 * License URI: http://www.apache.org/licenses/LICENSE-2.0
-	 * Version: 4.2.0
+	 * Version: 4.2.1
 	 * Text Domain: swedbank-pay-woocommerce-checkout
 	 * Domain Path: /languages
 	 * WC requires at least: 3.0.0
-	 * WC tested up to: 4.5.2
+	 * WC tested up to: 4.8.0
 	 */
 
 	use SwedbankPay\Checkout\WooCommerce\WC_Swedbank_Plugin;
