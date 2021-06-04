6d46d99efe13055a1d9ec96ae99dad8d9c3ca7c5
aait <info@aait.se>
2021-06-04 11:44:25 +0600

    upd WC

--- swedbank-pay-woocommerce-checkout.php
+++ swedbank-pay-woocommerce-checkout.php
@@ -11,7 +11,7 @@
  * Text Domain: swedbank-pay-woocommerce-checkout
  * Domain Path: /languages
  * WC requires at least: 3.0.0
- * WC tested up to: 5.0.0
+ * WC tested up to: 5.3.0
  */
 
 use SwedbankPay\Checkout\WooCommerce\WC_Swedbank_Plugin;
